cf2e5c0b3d9f7e9821300913873b5dda22fc71fa
Sisti-Anjana <anjanasisti2@gmail.com>
2025-12-16 06:14:37 -0500

    Fix Vercel API routing for backend

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 // Vercel serverless function wrapper for Express app
 const app = require('../server/index.js');
 
-// Export as Vercel serverless function
-// Vercel will call this for /api/* routes
-module.exports = app;
+// Export as Vercel serverless function handler
+// Vercel routes /api/* to this function
+module.exports = (req, res) => {
+  // Pass the request to Express app
+  return app(req, res);
+};
 
