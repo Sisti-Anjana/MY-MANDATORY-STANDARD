465f8e0851fafd0953d67b1fdf05583dbfbb56cc
Sisti-Anjana <anjanasisti2@gmail.com>
2025-12-16 06:02:28 -0500

    Fix API wrapper for Vercel backend

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // Vercel serverless function wrapper for Express app
 const app = require('../server/index.js');
 
+// Export as Vercel serverless function
+// Vercel will call this for /api/* routes
 module.exports = app;
 
